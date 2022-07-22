dcc742184f163da11b7fa6bf2874d89001e52b36
Yipxx94 <847418391@qq.com>
2022-07-22 20:31:45 +0800

    implement acapp acwing oauth2 login

---
+++ game/views/settings/acwing/acapp/receive_code.py
@@ -0,0 +1,66 @@
+from django.http import JsonResponse
+from django.core.cache import cache
+import requests    # 请求一个链接
+from django.contrib.auth.models import User
+from game.models.player.player import Player
+from random import randint
+
+def receive_code(request):
+    data = request.GET
+
+    if "errcode" in data:
+        return JsonResponse({
+            'result': "apply failed",
+            'errcode': data['errcode'],
+            'errmag': data['errmag'],
+        })
+    code = data.get('code')
+    state = data.get('state')
+
+    if not cache.has_key(state):
+        return JsonResponse({
+            'result': "state not exist",
+        })
+    cache.delete(state)
+
+    apply_access_token_url = "https://www.acwing.com/third_party/api/oauth2/access_token/"
+    params = {
+        'appid': "2848",
+        'secret': "0a8583215411416db28300b55b3117ca",
+        'code': code
+    }
+
+    access_token_res = requests.get(apply_access_token_url, params=params).json()
+    access_token = access_token_res['access_token']
+    openid = access_token_res['openid']
+
+    players = Player.objects.filter(openid=openid)    # 从数据库取得匹配的结果，返回一个对象列表
+    if players.exists():    # 如果该用户已存在，则无需重新获取信息，直接登录即可
+        player = players[0]
+        return JsonResponse({
+            'result': "success",
+            'username': player.user.username,
+            'photo': player.photo,
+        })
+
+    get_userinfo_url = "https://www.acwing.com/third_party/api/meta/identity/getinfo/"
+    params = {
+        "access_token": access_token,
+        "openid": openid
+    }
+
+    userinfo_res = requests.get(get_userinfo_url, params=params).json()
+    username = userinfo_res['username']
+    photo = userinfo_res['photo']
+
+    while User.objects.filter(username=username).exists():
+        username += str(randint(0, 9))
+
+    user = User.objects.create(username=username)
+    player = Player.objects.create(user=user, photo=photo)
+
+    return JsonResponse({
+        'result': "success",
+        'username': player.user.username,
+        'photo': player.photo,
+    })
